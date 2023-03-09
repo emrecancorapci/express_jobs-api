865780b03bfe64c608d3d1d0101cf17f3fe9c610
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-03-09 10:24:46 +0300

    error handler improvement

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -2,8 +2,30 @@ import { StatusCodes } from 'http-status-codes';
 import CustomAPIError from '../errors/custom-api.js';
 
 export default (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
+  const customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong. Please try again later.',
+  };
+
+  if (err instanceof CustomAPIError)
     return res.status(err.statusCode).json({ msg: err.message });
+
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(' ');
+    customError.statusCode = 400;
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+
+  if (err.name === 'CastError') {
+    customError.msg = `No item found with the id: ${err.value}`;
+    customError.statusCode = 404;
+  }
+
+  if (err.code && err.code === 11000)
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field. Please choose an unique value.`;
+
+  return res.status(customError.statusCode).json({ msg: customError.msg });
 };
